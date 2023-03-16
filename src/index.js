92bf829a44f2d3d6966c5ef26f26aff6dfb10820
Daniel <daniel_ye.nz@hotmail.com>
2023-03-15 20:37:48 -0700

    finish register and login for backend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
+import { userRouter } from './routes/users.js';
+
+dotenv.config();
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//endpoints
+app.use("/auth", userRouter);
+
+//connect to database
+mongoose.connect(process.env.MONGODB_CONNECTION);
 
 app.listen(3001, () => console.log("Server Started"));
